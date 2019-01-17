f615306cd8d99e9fc8530fc951d6e85762535a3f
jason-t-fu <jasonfu1992@gmail.com>
2019-01-17 15:51:35 -0800

    Implement different algorithms

---
+++ time_diff/time_diff.rb
@@ -0,0 +1,49 @@
+require 'byebug'
+def my_min1(list) #O(n^2)
+    list.each do |el|
+        return el if list.all? { |el2| el2 >= el }
+    end
+end
+
+def my_min2(list) #O(n)
+    min = list.first
+    list.each do |el|
+        min = el if el < min
+    end
+    min
+end
+
+def largest_contiguous_subsum1(list) # O(n^2)
+    sub_arrays = []
+    count = 1
+    
+    (1..list.length).each do |i|
+        sub_arrays += list.each_cons(i).to_a
+    end
+    sub_arrays.map { |sub_array| sub_array.sum }.max
+end
+
+def largest_contiguous_subsum2(list) # O(n)
+    curr_sum = list.first
+    global_sum = list.first
+    list[1..-1].each do |num|
+        curr_sum = [num, curr_sum + num].max
+        global_sum = [curr_sum, global_sum].max
+    end
+    global_sum
+end
+
+list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+p my_min1(list) 
+p my_min2(list)
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum1(list)
+
+list = [5, 3, -7]
+p largest_contiguous_subsum2(list)
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum2(list)
+list = [-5, -1, -3]
+p largest_contiguous_subsum2(list)
